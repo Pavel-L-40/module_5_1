bc9b13968e9f9a3d6b48427134c6a84d5022057d
PAvel <95528324+Pavel-L-40@users.noreply.github.com>
2024-09-16 22:21:08 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -6,15 +6,17 @@ class House:
 
     def go_too(self, new_floor):
         if self.numbers_of_floor <= new_floor or new_floor < 0:
-            print(f'в {self.name} такого этажа не существует!')
-        else: print(f'в {self.name} этаж есть')
+            print(f'в {self.name} {new_floor} этажа не существует!')
+        else:
+            print(self.name, end = ': ')
+            for i in range(new_floor): print(i+1, end =' ')
+            print()
 
 
 alp = House('ЖК Эльбрус', 30)
 alp.go_too(110)
 
-fav = House('ЖК Фавторит', 50)
-fav.go_too(110)
-
-fav.numbers_of_floor = 120
-fav.go_too(110)
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_too(5)
+h2.go_too(10)
